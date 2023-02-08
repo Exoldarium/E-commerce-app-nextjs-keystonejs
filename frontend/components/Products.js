732e741108958841c8b5f0709c33f7c5710aaa94
Dusan <shandoo91@gmail.com>
2023-02-08 17:59:47 +0100

    added first query

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,3 +1,30 @@
-export default function Products () {
-  return <div>Helloooo im a products page</div>
+import { gql, useQuery } from '@apollo/client';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    products {
+      id
+      name
+      description
+      price
+      photo {
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function Products() {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log({ data, error, loading });
+
+  return (
+    <div>
+      {data.products.map((product) => (
+        <p>{product.id}</p>
+      ))}
+    </div>
+  );
 }
