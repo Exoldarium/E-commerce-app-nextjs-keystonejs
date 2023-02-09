4ff6600953a5c0f038b56a8f6a22873dd5dc5edf
Dusan <shandoo91@gmail.com>
2023-02-09 12:19:38 +0100

    fixed styled components problem of not matching classes

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,26 +2,22 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(document) {
     const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+    const originalRenderPage = document.renderPage;
 
     try {
-      ctx.renderPage = () =>
+      document.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
+            // eslint-disable-next-line react/jsx-filename-extension, react/jsx-props-no-spreading
             sheet.collectStyles(<App {...props} />),
         });
 
-      const initialProps = await Document.getInitialProps(ctx);
+      const initialProps = await Document.getInitialProps(document);
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
     } finally {
       sheet.seal();
