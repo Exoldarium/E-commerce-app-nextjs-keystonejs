4ac5a1b0e8435059010f58f005db8faf5a5559c8
Dusan <shandoo91@gmail.com>
2023-03-10 17:13:47 +0100

    refactoring, added manage page

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -11,11 +11,28 @@ import { useUser } from './User';
 export default function Header() {
   const user = useUser();
   const cartItems = user?.cart;
-  const cartNotEmpty = cartItems.length >= 1;
-  const cartEmpty = cartItems.length < 1;
+  const cartNotEmpty = cartItems?.length >= 1;
+  const cartEmpty = cartItems?.length < 1;
   const { isMenuActive, toggleMenu, closeCart } = useSetState();
 
-  if (user || !user) {
+  if (!user) {
+    return (
+      <HeaderStyles>
+        <LogoStyles onClick={closeCart}>
+          <Link href="/products">It's a LOGO</Link>
+        </LogoStyles>
+        <Search />
+        <Link href="/cart">Cart</Link>
+        <Nav active={isMenuActive} onClick={toggleMenu} />
+        {/* button visible only under 790px */}
+        <MobileNavButtonStyles type="button" onClick={toggleMenu}>
+          Click
+        </MobileNavButtonStyles>
+      </HeaderStyles>
+    );
+  }
+
+  if (user) {
     return (
       <HeaderStyles>
         <LogoStyles onClick={closeCart}>
@@ -33,7 +50,6 @@ export default function Header() {
           </Link>
         )}
         {cartEmpty && <Link href="/cart">Cart</Link>}
-        {/* <CartMenu /> */}
         <Nav active={isMenuActive} onClick={toggleMenu} />
         {/* button visible only under 790px */}
         <MobileNavButtonStyles type="button" onClick={toggleMenu}>
---
+++ frontend/pages/manage.js
@@ -0,0 +1,8 @@
+// TODO
+// add a page that will allow user to manage their products
+// the page should display all products that a user currently has
+// the page should have update, create and delete options for products
+
+export default function ManagePage() {
+  return <div>Heloooo!</div>;
+}
