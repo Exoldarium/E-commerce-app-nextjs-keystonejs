578cd4d865057d0c87a9b43411cc1a2c6117e3c1
Dusan <shandoo91@gmail.com>
2023-03-13 15:48:11 +0100

    removed logs and comments

--- frontend/lib/ordersPaginationField.js
+++ frontend/lib/ordersPaginationField.js
@@ -2,9 +2,6 @@ export default function ordersPaginationField() {
   return {
     keyArgs: false,
     merge(existing, incoming, { args: { skip = 0 } }) {
-      console.log(existing, incoming);
-      // Slicing is necessary because the existing data is
-      // immutable, and frozen in development.
       const merged = existing ? existing.slice(0) : [];
       for (let i = 0; i < incoming.length; ++i) {
         merged[skip + i] = incoming[i];
